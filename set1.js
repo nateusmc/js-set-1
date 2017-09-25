a9469e9d75a8da84c687825cda4200a6a431df51
Nate Garrett <nate.garrett1992@gmail.com>
2017-09-25 13:46:38 -0500

    problem 3 complete

--- set1.js
+++ set1.js
@@ -5,8 +5,12 @@ console.log('Hi, my name is Chris and I\'m 29 years old');
 let whoAmI = function  (){
   console.log('Hi, my name is Chris and I\'m 29 years old');
 };
+whoAmI();
 //3
-
+let whoAmI = function (name, age) {
+	console.log(`Hi, my name is ${name} and I'm ${age} years old`);
+}
+whoAmI('Chris', 29);
 //4
 
 //5
