28d18326472bed7e628507f33d568af3a179fd02
Nate Garrett <nate.garrett1992@gmail.com>
2017-09-25 14:22:51 -0500

    solved question 5

--- set1.js
+++ set1.js
@@ -16,14 +16,26 @@
 //     let yearOfBirth = 2017 - age;
 //     console.log(`I was born in ${yearOfBirth}`)
 // }
-let whoAmI = function (name, age) {
-  let yearOfBirth = 2017 - age;
-  console.log(`Hi, my name is ${name} and I'm ${age} years old`);
-  console.log(`I was born in ${yearOfBirth}`)
-};
-whoAmI("Jesse", 27);
+// let whoAmI = function (name, age) {
+//   let yearOfBirth = 2017 - age;
+//   console.log(`Hi, my name is ${name} and I'm ${age} years old`);
+//   console.log(`I was born in ${yearOfBirth}`)
+// };
+// whoAmI("Jesse", 27);
 //5
 
+let yearOfBirth = function(age){
+	return 2017 - age;
+};
+
+let whoAmI = function (name, age) {
+	let yob = yearOfBirth(age);
+  console.log(`Hi, my name is ${name} and I'm ${age} years old`);
+  console.log(`I was born in ${yob}`);
+};
+
+
+whoAmI("Jesse", 27);
 //6
 
 //7
