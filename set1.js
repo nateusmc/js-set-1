15dd92e6c6b1ab902d16336bb3b63bcfaf8175d9
Nate Garrett <nate.garrett1992@gmail.com>
2017-09-25 15:38:05 -0500

    solved problem 7

--- set1.js
+++ set1.js
@@ -23,31 +23,53 @@
 // };
 // whoAmI("Jesse", 27);
 //5
-let yearOfBirth = function(age){ return 2017 - age; };
-let whoAmI = function (name, age) { let yob = yearOfBirth(age); console.log(Hi, my name is ${name} and I'm ${age} years old); console.log(I was born in ${yob}); };
-whoAmI("Jesse", 27);
+// let yearOfBirth = function(age) { 
+//   return 2017 - age; 
+// };
+// let whoAmI = function (name, age) { 
+//   let yob = yearOfBirth(age); 
+//   console.log(`Hi, my name is ${name} and I'm ${age} years old`); 
+//   console.log(`I was born in ${yob}`);
+// };
+// whoAmI("Jesse", 27);
 //6
-function yearOfBirth(age) {
-  return 2017 - age;
-}
-let whoAmI = function(name, age){
-  try {
-    if (age <= 0){
-      throw new Error('Your age must be a positive number');
-    }
-    const yob = yearOfBirth(age);
-    console.log(`Hi, my name is ${name} and I'm ${age} years old`);
-    console.log(`I was born in ${yob}`);
-  }
-  catch (e){
-    console.log(e);
-  }
-};
-whoAmI("Jesse", -27);
+// function yearOfBirth(age) {
+//   return 2017 - age;
+// }
+// let whoAmI = function(name, age){
+//   try {
+//     if (age <= 0){
+//       throw new Error('Your age must be a positive number');
+//     }
+//     const yob = yearOfBirth(age);
+//     console.log(`Hi, my name is ${name} and I'm ${age} years old`);
+//     console.log(`I was born in ${yob}`);
+//   }
+//   catch (e){
+//     console.log(e);
+//   }
+// };
+// whoAmI("Jesse", -27);
 //7
+// function yearOfBirth(age) {
+//   return 2017 - age;
+// }
+// let whoAmI = function(name, age){
+//   try {
+//     if (name || age === undefined){
+//       console.error('Argument not valid');
+//     }
+//     const yob = yearOfBirth(age);
+//     console.log(`Hi, my name is ${name} and I'm ${age} years old`);
+//     console.log(`I was born in ${yob}`);
+//   }
+//   catch (e){
+//     console.log(e);
+//   }
+// };
+// whoAmI();
 
+// function newFunction() {
+//     return console.error;
+// }
 //8
-
-//9
-
-//10
