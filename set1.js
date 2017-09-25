73f076b90c8dafc57536879dd78e61f497be9a8f
edemahruh <dev@jesseparrott.com>
2017-09-25 13:39:46 -0500

    Problem 2 finished

--- set1.js
+++ set1.js
@@ -1,7 +1,10 @@
+'use strict';
 //1
 console.log('Hi, my name is Chris and I\'m 29 years old');
 //2
-
+let whoAmI = function  (){
+  console.log('Hi, my name is Chris and I\'m 29 years old');
+};
 //3
 
 //4
